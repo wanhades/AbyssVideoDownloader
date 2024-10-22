eeee5890790b092dec038d5a7b07c3010325a901
abe <abdlhaymostaid72@gmail.com>
2024-10-22 01:26:46 +0100

    refactor: update regex

--- src/main/resources/tvphim.js
+++ src/main/resources/tvphim.js
@@ -6,7 +6,7 @@ function extractVideoID(jsCode) {
     while (!evaluatedString.includes(targetSubstring)) {
         evaluatedString = eval(evaluatedString.replaceAll("eval", ""));
     }
-    var match = evaluatedString.match(/link=https:\/\/short\.ink\/([a-zA-Z0-9]+)/);
+    var match = evaluatedString.match(/link=https:\/\/short\.ink\/([a-zA-Z0-9_\-@.]+)/);
 
     return match ? match[1] : ""
 }
