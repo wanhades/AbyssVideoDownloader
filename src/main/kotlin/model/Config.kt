725e5c00d0fbbffc8af7cec6da03e9233da571a3
abe <abdlhaymostaid72@gmail.com>
2024-10-22 20:27:24 +0100

    refactor: rename getVideoID to getVParameter

--- src/main/kotlin/model/Config.kt
+++ src/main/kotlin/model/Config.kt
@@ -1,12 +1,12 @@
 package com.abmo.model
 
-import util.getVideoID
+import util.getVParameter
 import util.systemDownloadFolder
 import java.io.File
 
 data class Config(
     val url: String,
     val resolution: String,
-    var outputFile: File? = File(systemDownloadFolder(), "${url.getVideoID()}_$resolution.mp4"),
+    var outputFile: File? = File(systemDownloadFolder(), "${url.getVParameter()}_$resolution.mp4"),
     val header: Map<String, String>? = null
 )
