b2903fcf098ac20eeb1199b729d0dfc6570bd8a4
abe <abdlhaymostaid72@gmail.com>
2024-10-24 03:25:52 +0100

    feat: implement parallel download functionality

--- src/main/kotlin/com/abmo/model/Config.kt
+++ src/main/kotlin/com/abmo/model/Config.kt
@@ -1,12 +1,11 @@
 package com.abmo.model
 
-import com.abmo.util.getVParameter
-import com.abmo.util.systemDownloadFolder
 import java.io.File
 
 data class Config(
     val url: String,
     val resolution: String,
-    var outputFile: File? = File(systemDownloadFolder(), "${url.getVParameter()}_$resolution.mp4"),
-    val header: Map<String, String>? = null
+    var outputFile: File?,
+    val header: Map<String, String>? = null,
+    val connections: Int = 6
 )
--- src/main/kotlin/com/abmo/services/VideoDownloader.kt
+++ src/main/kotlin/com/abmo/services/VideoDownloader.kt
@@ -4,17 +4,77 @@ import com.abmo.model.*
 import com.abmo.util.CryptoHelper
 import com.abmo.util.displayProgressBar
 import com.abmo.util.toJson
+import com.abmo.util.toReadableTime
 import com.mashape.unirest.http.Unirest
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-
+import kotlinx.coroutines.sync.Semaphore
+import kotlinx.coroutines.sync.withPermit
+import java.io.File
 
 class VideoDownloader(
     private val cryptoHelper: CryptoHelper
 ) {
 
+    suspend fun downloadSegmentsInParallel(config: Config, videoMetadata: Video?) {
+        val simpleVideo = videoMetadata?.toSimpleVideo(config.resolution)
+        val segmentBodies = generateSegmentsBody(simpleVideo)
+        val segmentUrl = getSegmentUrl(videoMetadata)
+        val decryptionKey = cryptoHelper.getKey(simpleVideo?.size)
+
+        val tempFolder = File(config.outputFile?.parentFile, "temp_${simpleVideo?.slug}_${simpleVideo?.label}")
+        // no need to check if path exists before creating temp folder we already did that in Main.kt (why do I use WE I'M ALONE AT THIS)
+        tempFolder.mkdir()
+
+
+        // reference: https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.sync/-semaphore/
+        // used to limit the number of concurrent coroutines executing the download tasks.
+        val semaphore = Semaphore(config.connections)
+        val totalSegments = segmentBodies.size
+        var downloadedSegments = 0
+        var totalBytesDownloaded = 0L
+
+        val startTime = System.currentTimeMillis()
+
+        coroutineScope {
+            val downloadJobs = segmentBodies.mapIndexed { i, segmentBody ->
+                async {
+                    semaphore.withPermit {
+                        var isFirst = true
+                        requestSegment(segmentUrl, segmentBody.toJson()).collect { chunk ->
+                            val array = if (isFirst) {
+                                isFirst = false
+                                cryptoHelper.decryptAESCTR(chunk, decryptionKey)
+                            } else {
+                                chunk
+                            }
+                            File(tempFolder, "segment_$i").appendBytes(array)
+                            totalBytesDownloaded += array.size
+                        }
+                    }
+                    downloadedSegments += 1
+                    displayProgressBar(i + 1, totalSegments, totalBytesDownloaded, downloadedSegments, startTime)
+                }
+            }
+            downloadJobs.awaitAll()
+        }
+        val endTime = System.currentTimeMillis()
+        val duration = (endTime - startTime).toReadableTime()
+        println("\n\nDownload took: $duration")
+        println("\nAll segments have been downloaded successfully!")
+        println("merging segments into mp4 file...")
+        config.outputFile?.let { mergeSegmentsIntoMp4(tempFolder, it) }
+
+    }
+
+
+    @Deprecated("Use downloadSegmentsInParallel instead.",
+        ReplaceWith("downloadSegmentsInParallel(config, videoMetadata)"))
     suspend fun downloadVideo(config: Config, videoMetadata: Video?) {
         var totalBytesDownloaded = 0L
         val startTime = System.currentTimeMillis()
@@ -37,7 +97,41 @@ class VideoDownloader(
                 displayProgressBar(totalBytesDownloaded, simpleVideo?.size!!.toLong(), startTime)
             }
         }
+        val endTime = System.currentTimeMillis()
+        val duration = (endTime - startTime).toReadableTime()
+        println("\nDownload took: $duration")
         println("\nDownload complete")
+    }
+
+
+    private fun mergeSegmentsIntoMp4(segmentFolderPath: File, output: File) {
+        val segmentFiles  = segmentFolderPath.listFiles { file -> file.name.startsWith("segment_") }
+            ?.toList()
+            ?.sortedBy { it.name.removePrefix("segment_").toIntOrNull() }
+            ?: emptyList()
+        segmentFiles.forEach {
+            output.appendBytes(it.readBytes())
+        }
+
+        println("merge complete successfully")
+
+        if (segmentFolderPath.exists() && segmentFolderPath.isDirectory) {
+            val files = segmentFolderPath.listFiles()
+
+            if (files != null) {
+                for (file in files) {
+                    file.delete()
+                }
+            }
+
+            if (segmentFolderPath.delete()) {
+                println("Deleted temporary folder at: ${segmentFolderPath.absolutePath}")
+            } else {
+                println("Failed to delete folder: ${segmentFolderPath.absolutePath}")
+            }
+        } else {
+            println("Folder does not exist or is not a directory: ${segmentFolderPath.absolutePath}")
+        }
     }
 
 
