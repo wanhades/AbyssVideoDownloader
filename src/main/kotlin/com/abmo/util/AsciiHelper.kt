cba840c169ce66f21a712ac4b6ee88840954d433
abe <abdlhaymostaid72@gmail.com>
2024-10-24 03:19:47 +0100

    add ASCII progress bar for parallel downloads

--- src/main/kotlin/com/abmo/util/AsciiHelper.kt
+++ src/main/kotlin/com/abmo/util/AsciiHelper.kt
@@ -5,13 +5,29 @@ fun displayProgressBar(bytesDownloaded: Long, totalSize: Long, startTime: Long) 
     val progress = (bytesDownloaded.toDouble() / totalSize.toDouble()) * 100
     val barLength = 50
     val filledLength = (progress / 100 * barLength).toInt()
-    val bar = "=".repeat(filledLength) + "-".repeat(barLength - filledLength)
+    val bar = "█".repeat(filledLength) + " ".repeat(barLength - filledLength)
     val formattedProgress = String.format("%.2f", progress)
     val elapsedTime = (System.currentTimeMillis() - startTime) / 1000.0
     val downloadSpeed = if (elapsedTime > 0) (bytesDownloaded / elapsedTime) else 0.0
     val formattedSpeed = String.format("%.2f KB/s", downloadSpeed / 1024.0)
 
     print("\rProgress: |$bar| $formattedProgress% - Speed: $formattedSpeed")
+    if (progress >= 100) {
+        println()
+    }
+}
+
+fun displayProgressBar(current: Int, totalSegments: Int, bytesDownloaded: Long, totalDownloaded: Int, startTime: Long) {
+    val progress = totalDownloaded.toDouble() / totalSegments
+    val barLength = 50
+    val filledLength = (progress * barLength).toInt()
+    val bar = "█".repeat(filledLength) + " ".repeat(barLength - filledLength)
+
+    val elapsedTime = (System.currentTimeMillis() - startTime) / 1000.0
+    val downloadSpeed = if (elapsedTime > 0) (bytesDownloaded / elapsedTime) else 0.0
+    val formattedSpeed = String.format("%.2f KB/s", downloadSpeed / 1024.0)
+
+    print("\rCurrent Segment $current |$bar| Segment $totalDownloaded/$totalSegments (${(progress * 100).toInt()}%) - Speed: $formattedSpeed")
     if (progress >= 100) {
         println()
     }
