a776bead0cb34c2e8f5607a803d40b8c61eddc66
abe <abdlhaymostaid72@gmail.com>
2024-11-06 03:32:48 +0100

    correct data retrieval bug in SieutamphimProvider implementation

--- src/main/kotlin/com/abmo/providers/SieutamphimProvider.kt
+++ src/main/kotlin/com/abmo/providers/SieutamphimProvider.kt
@@ -3,23 +3,22 @@ package com.abmo.providers
 import com.abmo.util.fetchDocument
 import java.nio.charset.Charset
 
-
 class SieutamphimProvider: Provider {
-
 
     override fun getVideoID(url: String): String? {
         val splits = url.split("--")
         val originalUrl = splits[0]
-        val episodeInput = splits.getOrNull(1)?.toIntOrNull() ?: 0
+        val episodeInput = (splits.getOrNull(1)?.toIntOrNull()?.minus(1)) ?: 0
         val document = originalUrl.fetchDocument()
-        val episodes = document.select("div#mytick button span")
-        if (episodeInput > episodes.size) return null
+        val episodes = document.select("div#mytick span.server-hx").nextAll()
+            .select("button span").ifEmpty { return null }
+
+        if (episodeInput >= episodes.size) return null
 
         val encryptedUrl = episodes[episodeInput].attr("data-src")
         val regex = """const\s+key\s*=\s*(\d+);""".toRegex()
         val key = regex.find(document.html())?.groupValues?.getOrNull(1)
             ?.toIntOrNull() ?: return null
-
         val videoID = decodeXor(encryptedUrl, key).substringAfterLast("/")
 
         return videoID
