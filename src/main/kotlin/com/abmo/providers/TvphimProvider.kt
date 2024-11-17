25db9cd290569ea3a88da3d402ef937a9fdad40d
abe <abdlhaymostaid72@gmail.com>
2024-11-17 23:41:55 +0100

    Fix SSLHandshakeException by forcing TLS 1.3 protocol for HTTP client

--- src/main/kotlin/com/abmo/providers/TvphimProvider.kt
+++ src/main/kotlin/com/abmo/providers/TvphimProvider.kt
@@ -2,6 +2,10 @@ package com.abmo.providers
 
 import com.abmo.executor.JavaScriptExecutor
 import com.abmo.util.fetchDocument
+import com.mashape.unirest.http.Unirest
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory
+import org.apache.http.impl.client.HttpClients
+import org.apache.http.ssl.SSLContexts
 
 
 class TvphimProvider(
@@ -9,6 +13,7 @@ class TvphimProvider(
 ): Provider {
 
     override fun getVideoID(url: String): String? {
+        configureTls13Protocol()
         val hProUrl = url.fetchDocument()
             .select("a[title=\"Server H.PRO\"]").attr("href")
         if (hProUrl.isNullOrBlank()) return null
@@ -23,6 +28,20 @@ class TvphimProvider(
             jsCode)
 
         return videoID
+    }
+
+    private fun configureTls13Protocol() {
+        val sslContext = SSLContexts.custom()
+            .useProtocol("TLSv1.2")
+            .build()
+
+        val tlsSocketFactory = SSLConnectionSocketFactory(sslContext)
+
+        val httpClient = HttpClients.custom()
+            .setSSLSocketFactory(tlsSocketFactory)
+            .build()
+
+        Unirest.setHttpClient(httpClient)
     }
 
 }
